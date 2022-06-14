85472918fd1c46e868fb43a5bd5aa64ae51addb2
Rishabh122333 <102612221+Rishabh122333@users.noreply.github.com>
2022-06-14 18:13:33 +0530

    Added solution - LeetHub

--- First non-repeating character in a stream - GFG/first-nonrepeating-character-in-a-stream.java
+++ First non-repeating character in a stream - GFG/first-nonrepeating-character-in-a-stream.java
@@ -26,37 +26,22 @@ class Solution
     {
         StringBuilder resultantString = new StringBuilder();
     int[] characterFrequency = new int[26];
-
-    // queue to store the characters
     Queue<Character> queue = new LinkedList<Character>();
-
-    // traverse whole stream of characters
     for (int i = 0; i < str.length(); i++) {
       char ch = str.charAt(i);
-
-      // push the character into the queue
       queue.add(ch);
-
-      // increment the frequency count
       characterFrequency[ch - 'a']++;
-
-      // check for the non repeating character
       while (!queue.isEmpty()) {
-        // when the character is repeated
         if (characterFrequency[queue.peek() - 'a'] > 1)
           queue.remove();
-        // when the character is not repeating
         else {
           resultantString.append(queue.peek());
           break;
         }
       }
-
-      // if there is no non-repeating character
       if (queue.isEmpty())
         resultantString.append("#");
     }
-    
     return resultantString.toString();
     }
 }
