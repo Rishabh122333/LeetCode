7f612ac131761ab026831235fe34886bbe035490
Rishabh122333 <102612221+Rishabh122333@users.noreply.github.com>
2022-07-18 21:43:33 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (94.15%) - LeetHub

---
+++ 1854-maximum-population-year/1854-maximum-population-year.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maximumPopulation(int[][] logs) {
+         int [] arr = new int[101];
+    for(int i = 0;i<logs.length ;i++)
+    {
+       
+      
+            arr[logs[i][0]-1950]++;
+            arr[logs[i][1]-1950]--;
+       
+    }
+    for(int i = 1;i<101;i++)
+    {
+        arr[i]=arr[i-1]+arr[i];
+    }
+    int max=0,ans=1950;
+    for(int i = 0;i<101;i++)
+    {
+        if(arr[i]>max){
+            max = arr[i];
+            ans=i+1950;
+        }
+    }
+    return ans;
+    }
+}
