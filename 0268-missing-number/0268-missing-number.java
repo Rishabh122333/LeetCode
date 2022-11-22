f21c59484719540bedc66281fa46b2c6fadec0e7
Rishabh122333 <102612221+Rishabh122333@users.noreply.github.com>
2022-11-23 01:45:54 +0530

    Time: 1 ms (88.19%), Space: 50.9 MB (58.72%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,20 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int max=0,sum=0,total=0,min=Integer.MAX_VALUE;
+        int max=nums.length,total=0;
+        total=max*(max+1)/2;
         for(int i=0;i<nums.length;i++)
         {
-            max=Math.max(max,nums[i]);
-            min=Math.min(min,nums[i]);
-            sum+=nums[i];
+            total-=nums[i];
         }
-        if(min!=0)
-            return 0;
-        total=max*(max+1)/2;
-        if(total==sum)
-        {
-            return max+1;
-        }
-        else
-            return total-sum;
+        return total;
     }
 }
