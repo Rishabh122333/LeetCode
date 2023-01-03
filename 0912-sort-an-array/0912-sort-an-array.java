bd1dc28191912f4d166a69e4642fe30b8c118fca
RISHABH ROCHLANI <102612221+Rishabh122333@users.noreply.github.com>
2023-01-03 09:23:19 -0800

    Time: 14 ms (97.69%), Space: 77.3 MB (60.79%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,56 +1,37 @@
 class Solution {
-    public int[] sortArray(int[] arr) {
-        sort(arr, 0, arr.length-1);
-        return arr;
-        
-    }
-    public static void merge(int arr[], int l, int m, int r)
+    public int[] sortArray(int[] nums) {
+         int min=Integer.MAX_VALUE;
+    int max=Integer.MIN_VALUE;
+    
+    for(int i=0;i<nums.length;i++)
     {
-        int n1 = m - l + 1;
-        int n2 = r - m;
-        int L[] = new int [n1];
-        int R[] = new int [n2];
-        for (int i=0; i<n1; ++i)
-            L[i] = arr[l + i];
-        for (int j=0; j<n2; ++j)
-            R[j] = arr[m + 1+ j];
-        int i = 0, j = 0;
-        int k = l;
-        while (i < n1 && j < n2)
+        min=Math.min(min,nums[i]);
+        max=Math.max(max,nums[i]);
+    }
+    
+    int feq[]=new int[max-min+1];
+    
+    for(int i=0;i<nums.length;i++)
+        feq[nums[i]-min]++;
+    
+    
+    int ans[]=new int[nums.length];
+    int k=0;
+    for(int i=0;i<feq.length;i++)
+    {
+        if(feq[i]>0)
         {
-            if (L[i] <= R[j])
+            while(feq[i]>0)
             {
-                arr[k] = L[i];
-                i++;
+                ans[k]=i+min;
+                feq[i]--;
+                k++;
             }
-            else
-            {
-                arr[k] = R[j];
-                j++;
-            }
-            k++;
-        }
-        while (i < n1)
-        {
-            arr[k] = L[i];
-            i++;
-            k++;
-        }
-        while (j < n2)
-        {
-            arr[k] = R[j];
-            j++;
-            k++;
         }
     }
-   public static void sort(int arr[], int l, int r)
-    {
-        if (l < r)
-        {
-            int m = (l+r)/2;
-            sort(arr, l, m);
-            sort(arr , m+1, r);
-            merge(arr, l, m, r);
-        }
+    
+    return ans;
+    
+    
     }
 }
