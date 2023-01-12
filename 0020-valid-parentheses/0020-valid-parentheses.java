f610be7336af9790e2534b12e950904377fee4d0
RISHABH ROCHLANI <102612221+Rishabh122333@users.noreply.github.com>
2023-01-13 03:05:36 +0530

    Time: 2 ms (74.51%), Space: 40.2 MB (89.41%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if(ch=='(' || ch=='{' || ch=='[')
+                st.push(ch);
+            if(st.isEmpty())
+                return false;
+            else if(ch==')')
+            {
+                if(st.pop()!='(')
+                    return false;
+            }
+            else if(ch=='}')
+            {
+                if(st.pop()!='{')
+                    return false;
+            }
+            else if(ch==']')
+            {
+                if(st.pop()!='[')
+                    return false;
+            }
+        }
+        return st.isEmpty();
+    }
+}
