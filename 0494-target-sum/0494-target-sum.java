260d866ae73d029a58ffdd95c3adf5466d19ba46
RISHABH ROCHLANI <102612221+Rishabh122333@users.noreply.github.com>
2023-01-18 18:42:37 +0530

    Time: 4 ms (96.16%), Space: 42.1 MB (54.79%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,10 +1,8 @@
-// Let sum of subset 1 be s1 and subset 2 with s2
-// s1 - s2 = diff (given)
-// s1 + s2=sum of array (logical)
-// Therefore adding both eq we get :
-// 2s1= diff + sum of array
-// s1= (diff + sum of array)/2;
-// Problem reduces to find no of subsets with given sum
+// Suppose the array is partiotioned in two subsets with sum S1 and S2, so we know that,  
+// S1 + S2  is the total sum array nums 
+// S1 – S2 is the given diff
+// Subtracting the two equations we would get,
+// sum – diff = (S1 + S2) – (S1 – S2) = 2*S2 . So, S2 = ( sum – diff ) / 2
 
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
