071fa2dd2ab59ac5d253778e6dcb41c7070329e3
RISHABH ROCHLANI <102612221+Rishabh122333@users.noreply.github.com>
2023-01-23 15:27:24 +0530

    Time: 19 ms (39.08%), Space: 49.3 MB (20.44%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -25,13 +25,14 @@ class Solution {
         int m=x.length();
         int n=y.length();
         int t[][]=new int [m+1][n+1];
-        
-        for (int j = 0; j < n+1; j++)
-            t[0][j] = 0;
-
-        for (int i = 1; i < m+1; i++)
-            t[i][0] = 0;
-
+        for(int i=0;i<m+1;i++)
+        { 
+            for(int j=0;j<n+1;j++)
+            {
+                if(i==0 || j==0)
+                    t[i][j]=0;
+            }
+        }
         for(int i=1;i<m+1;i++)
         { 
             for(int j=1;j<n+1;j++)
