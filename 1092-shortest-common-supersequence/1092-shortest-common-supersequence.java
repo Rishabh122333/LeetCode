2993cf967a3e31769095c2ab5248a59e5b709fab
RISHABH ROCHLANI <102612221+Rishabh122333@users.noreply.github.com>
2023-01-23 23:20:48 +0530

    Time: 28 ms (35.80%), Space: 46.6 MB (36.43%) - LeetHub

---
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
@@ -0,0 +1,62 @@
+class Solution {
+    public String shortestCommonSupersequence(String x, String y) {
+        int m=x.length();
+        int n=y.length();
+        int t[][]=new int [m+1][n+1];
+        StringBuilder sb= new StringBuilder();
+        for(int i=0;i<m+1;i++)
+        { 
+            for(int j=0;j<n+1;j++)
+            {
+                if(i==0 || j==0)
+                    t[i][j]=0;
+            }
+        }
+        for(int i=1;i<m+1;i++)
+        { 
+            for(int j=1;j<n+1;j++)
+            {
+                if(x.charAt(i-1)==y.charAt(j-1))
+                {
+                    t[i][j]=1+t[i-1][j-1];
+                }
+                else
+                {
+                    t[i][j]=Math.max(t[i][j-1],t[i-1][j]);
+                }
+            }
+        }
+        int i=m,j=n;
+        while(i>0 && j>0)
+        {
+          if(x.charAt(i-1)==y.charAt(j-1))
+            {
+                sb.append(x.charAt(i-1));
+                i--;
+                j--;
+            }
+            else if(t[i-1][j]>t[i][j-1])
+            {
+                sb.append(x.charAt(i-1));
+                i--;
+            }
+            else
+            {
+                sb.append(y.charAt(j-1));
+                j--;
+            }
+        }
+        while(i>0)
+        {
+            sb.append(x.charAt(i-1));
+            i--;
+            
+        }
+        while(j>0)
+        {
+            sb.append(y.charAt(j-1));
+            j--;
+        }
+        return sb.reverse().toString();
+    }
+}
