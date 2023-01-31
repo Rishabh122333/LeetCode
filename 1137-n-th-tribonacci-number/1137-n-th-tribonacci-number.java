969946a75779e84c8ce5cfaeaca656dd8c810c71
RISHABH ROCHLANI <102612221+Rishabh122333@users.noreply.github.com>
2023-02-01 02:23:46 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (21.29%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -9,22 +9,41 @@
 //     }
 // }
 
+//dp sol
+// class Solution {
+//     public int tribonacci(int n) {
+//         if(n<2)
+//             return n;
+//         if(n==2)
+//             return 1;
+//         int dp[]=new int[n+1];
+//         dp[0]=0;
+//         dp[1]=1;
+//         dp[2]=1;
+//         for(int i=3;i<=n;i++)
+//         {
+//            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+//         }
+        
+//         return dp[n];
+//     }
+// }
 
+//best sol
 class Solution {
     public int tribonacci(int n) {
         if(n<2)
             return n;
         if(n==2)
             return 1;
-        int dp[]=new int[n+1];
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=1;
+        int a=0,b=1,c=1;
         for(int i=3;i<=n;i++)
         {
-           dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+            int sum=a+b+c;
+            a=b;
+            b=c;
+            c=sum;
         }
-        
-        return dp[n];
+        return c;
     }
 }
