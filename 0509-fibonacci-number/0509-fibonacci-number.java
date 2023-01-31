62285e0dd48025e4673aa37d61d175f8b67b6b3b
RISHABH ROCHLANI <102612221+Rishabh122333@users.noreply.github.com>
2023-02-01 01:34:02 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (69.03%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,7 +1,24 @@
+//recursive approach
+// class Solution {
+//     public int fib(int n) {
+//         if(n<2)
+//             return n;
+//         return fib(n-1)+fib(n-2);
+//     }
+// }
+
+//dp sol
 class Solution {
     public int fib(int n) {
         if(n<2)
             return n;
-        return fib(n-1)+fib(n-2);
+        int dp[]=new int [n+1];
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 }
