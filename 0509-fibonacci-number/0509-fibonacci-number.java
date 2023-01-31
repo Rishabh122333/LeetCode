594a6e2f65609db59f052d23b808ea9c1fa4b27e
RISHABH ROCHLANI <102612221+Rishabh122333@users.noreply.github.com>
2023-02-01 01:37:39 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (50.50%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -8,17 +8,33 @@
 // }
 
 //dp sol
+// class Solution {
+//     public int fib(int n) {
+//         if(n<2)
+//             return n;
+//         int dp[]=new int [n+1];
+//         dp[0]=0;
+//         dp[1]=1;
+//         for(int i=2;i<=n;i++)
+//         {
+//             dp[i]=dp[i-1]+dp[i-2];
+//         }
+//         return dp[n];
+//     }
+// }
+
+//best sol 
 class Solution {
     public int fib(int n) {
         if(n<2)
             return n;
-        int dp[]=new int [n+1];
-        dp[0]=0;
-        dp[1]=1;
+        int a=0,b=1;
         for(int i=2;i<=n;i++)
         {
-            dp[i]=dp[i-1]+dp[i-2];
+            int sum=a+b;
+            a=b;
+            b=sum;
         }
-        return dp[n];
+        return b;
     }
 }
