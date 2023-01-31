c71d2e736b4101cf29dfcc1b33ab782c8e9e4026
RISHABH ROCHLANI <102612221+Rishabh122333@users.noreply.github.com>
2023-02-01 02:13:40 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (74.68%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -10,18 +10,35 @@
 // }
 
 //dp sol
+// class Solution {
+//     public int climbStairs(int n) {
+//         if(n<2)
+//             return n;
+//         int dp[]=new int [n+1];
+//         dp[0]=0;
+//         dp[1]=1;
+//         dp[2]=2;
+//         for(int i=3;i<=n;i++)
+//         {
+//             dp[i]=dp[i-1]+dp[i-2];
+//         }
+//         return dp[n];
+//     }
+// }
+
+//best sol
 class Solution {
     public int climbStairs(int n) {
-        if(n<2)
+        if(n<=2)
             return n;
-        int dp[]=new int [n+1];
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=2;
+        int a=1,b=2;
         for(int i=3;i<=n;i++)
         {
-            dp[i]=dp[i-1]+dp[i-2];
+            int sum=a+b;
+            a=b;
+            b=sum;
+            
         }
-        return dp[n];
+        return b;
     }
 }
