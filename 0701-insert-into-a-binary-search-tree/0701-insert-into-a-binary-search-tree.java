2e901db958aea4f4cc8511a6dd8540b76ee0d639
RISHABH ROCHLANI <102612221+Rishabh122333@users.noreply.github.com>
2023-02-02 09:33:15 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (56.90%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.java
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.java
@@ -13,39 +13,55 @@
  *     }
  * }
  */
+//recursive
 class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
         if(root==null)
             return new TreeNode(val);
-        TreeNode curr=root;
-        while(true)
-        {
-            if(val<curr.val)
-            {
-                if(curr.left!=null)
-                {
-                    curr=curr.left;
-                }
-                else
-                {
-                    curr.left=new TreeNode(val);
-                    break;
-                }
-            }
-            else{
-                if(curr.right!=null)
-                {
-                    curr=curr.right;
-                }
-                else
-                {
-                    curr.right=new TreeNode(val);
-                    break;
-                }
-            }
-                
-        }
+        if(root.val>val)
+            root.left=insertIntoBST(root.left, val);
+        else
+            root.right=insertIntoBST(root.right, val);
         return root;
         
     }
 }
+
+// level order 
+
+// class Solution {
+//     public TreeNode insertIntoBST(TreeNode root, int val) {
+//         if(root==null)
+//             return new TreeNode(val);
+//         TreeNode curr=root;
+//         while(true)
+//         {
+//             if(val<curr.val)
+//             {
+//                 if(curr.left!=null)
+//                 {
+//                     curr=curr.left;
+//                 }
+//                 else
+//                 {
+//                     curr.left=new TreeNode(val);
+//                     break;
+//                 }
+//             }
+//             else{
+//                 if(curr.right!=null)
+//                 {
+//                     curr=curr.right;
+//                 }
+//                 else
+//                 {
+//                     curr.right=new TreeNode(val);
+//                     break;
+//                 }
+//             }
+                
+//         }
+//         return root;
+        
+//     }
+// }
