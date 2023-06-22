6f1a3f0a792895179924be66f86f9e40b0b174a5
RISHABH ROCHLANI <102612221+Rishabh122333@users.noreply.github.com>
2023-06-23 00:04:16 +0530

    Update 0015-3sum.java

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,38 +1,32 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-         Arrays.sort(nums);
-        
-        List<List<Integer>> ans = new ArrayList<>(); 
-        
-        for (int i = 0; i < nums.length-2; i++) {
-            
-            if (i == 0 || (i > 0 && nums[i] != nums[i-1])) {
-                
-                int lo = i+1, hi = nums.length-1, target= 0 - nums[i];
-                
-                while (lo < hi) {
-                    if (nums[lo] + nums[hi] == target) {
-                        
-                        ans.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
-                        
-                        while (lo < hi && nums[lo] == nums[lo+1]) 
-                            lo++;
-                        while (lo < hi && nums[hi] == nums[hi-1])
-                            hi--;
-                        
-                        lo++; 
-                        hi--;
-                    } 
-                    else if (nums[lo] + nums[hi] < target) 
-                        lo++;
-                    
-                    else 
-                        hi--;
-               }
+        Arrays.sort(nums);
+        int n=nums.length;
+        List<List<Integer>> ans=new ArrayList<>();
+        for(int i=0;i<n;i++)
+        {
+            if(i>0 && nums[i]==nums[i-1]) // increaing i for all already calculated elements 
+                continue;
+            int j=i+1,k=n-1;
+            while(j<k)
+            {
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum==0)
+                {
+                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1])
+                        j++;
+                    while(j<k && nums[k]==nums[k+1])
+                        k--;
+                }
+                else if(sum<0)
+                    j++;
+                else
+                    k--;
             }
         }
         return ans;
-        
-        
     }
 }
