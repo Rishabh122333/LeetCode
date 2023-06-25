3de19aa26427a1b811f7cc236cf8ae4cf860b2c9
RISHABH ROCHLANI <102612221+Rishabh122333@users.noreply.github.com>
2023-06-26 00:10:35 +0530

    Update 0062-unique-paths.java

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,46 +1,20 @@
-// class Solution {
-//     public int uniquePaths(int m, int n) {
-//         return path(m,n,m-1,n-1);
-//     }
-//     public static int path(int m,int n, int i,int j)
-//     {
-//         if(i==0 && j==0)
-//         {
-//             return 1;
-//         }
-//         if(i<0 || j<0)
-//         {
-//             return 0;
-//         }
-//         int up=path(m,n,i-1,j);
-//         int left=path(m,n,i,j-1);
-//         return up+left;
-//     }
-// }
-
 class Solution {
     public int uniquePaths(int m, int n) {
         int dp[][]=new int [m][n];
-        dp[0][0]=1;
-        
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                 if(i==0 && j==0)
-                     dp[0][0]=1;
-                 else 
-                 {
-                     int up=0,left=0;
-                      if(i>0)
-                        up=dp[i-1][j];
-                     if(j>0)
-                        left=dp[i][j-1];
-                     dp[i][j]=up+left;
-                 }
-            }
-            
-        }
-        return dp[m-1][n-1];
+        for(int []a:dp)
+            Arrays.fill(a,-1);
+        return calculate(m,n,0,0,dp);
+    }
+    public static int calculate(int m,int n,int i, int j,int [][]dp)
+    {
+        if(i==m-1 && j==n-1)
+            return 1;
+        if(i>= m || j>=n)
+            return 0;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        int down =calculate(m,n,i+1,j,dp);
+        int right=calculate(m,n,i,j+1,dp);
+        return dp[i][j]=right +down;
     }
 }
