7db85fc71a96c301391fe816201fc6a3207a3a8c
RISHABH ROCHLANI <102612221+Rishabh122333@users.noreply.github.com>
2023-09-30 18:11:02 +0530

    Time: 1 ms (90.11%), Space: 44.7 MB (37.35%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,12 +1,14 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
         boolean flag=false;
-        for(int i=0;i<matrix.length;i++)
+        int m=matrix.length,n=matrix[0].length;
+        for(int i=0;i<m;i++)
         {
             if(matrix[i][0]==0)
-                flag=true;
-            for(int j=1;j<matrix[0].length;j++)
+                    flag=true;
+            for(int j=1;j<n;j++)
             {
+                
                 if(matrix[i][j]==0)
                 {
                     matrix[i][0]=0;
@@ -14,18 +16,15 @@ class Solution {
                 }
             }
         }
-        for(int i=matrix.length-1;i>=0;i--)
+        for(int i=m-1;i>=0;i--)
         {
-            for(int j=matrix[0].length-1;j>0;j--)
+            for(int j=n-1;j>0;j--)
             {
                 if(matrix[i][0]==0 || matrix[0][j]==0)
-                {
                     matrix[i][j]=0;
-                }
             }
-             if(flag)
-            matrix[i][0]=0;
+            if(flag)
+                matrix[i][0]=0;
         }
-        
     }
 }
